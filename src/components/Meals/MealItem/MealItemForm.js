06891613ca5b9625a998b123f4704ae2650db1aa
MuskanBh <muskanbharalawala10@gmail.com>
2023-02-24 20:34:54 +0530

    using ContextAPI

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,22 @@
+import { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 export default function MealItemForm(props){
+  const amountInpuRef = useRef();
+  const onSubmitHandler =(event)=>{
+    event.preventDefault();
+// current value is always a string
+    const enteredAmount = amountInpuRef.current.value;
+// to make it a number
+    const enteredAmountNumber = + enteredAmount;
+
+  }
   return(
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <Input label='Amount'
       input={{
+          ref:{amountInpuRef},
           id: 'amount_' + props.id,
           type: 'number',
           min: '1',
